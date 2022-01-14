c00ea8f0af366c68f37138861898845ff07cffc3
josemasf <josemasf@gmail.com>
2022-01-15 00:08:00 +0100

    add items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 import List from './components/List';
 
 function App() {
-  return (
+  return (    
     <List />
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,24 +1,49 @@
-import  {Component} from 'react';
+import  React, {Component} from 'react';
 import Loading from './Loading';
+import Item from './Item';
+import Header from './Header';
 
 class List extends Component{
     constructor(props){
         super(props)
         this.state ={
-            isLoading: false
+            isLoading: false, 
+            videos: null
         }
     }
     componentDidMount(){
         this.setState({isLoading: true});
         setTimeout(()=>{
-            this.setState({isLoading:false});
+            this.setState({isLoading:false, videos:[{  
+                id:0,
+                title:'¿Qué es CodelyTV? 🍄🔝 - Formación para programadores y divulgación del mundo del desarrollo',
+                url:'https://www.youtube.com/watch?v=rpMQd2DazTc',
+                thumbnail:'https://img.youtube.com/vi/rpMQd2DazTc/maxresdefault.jpg',
+              },
+              {   
+                id:1,
+                title:'Introducción a PHP: Cómo configurar tu entorno de desarrollo 🐘',
+                url: 'https://www.youtube.com/embed/watch?v=v2IjMrpZog4',
+                thumbnail: 'https://img.youtube.com/vi/v2IjMrpZog4/maxresdefault.jpg',
+              }]});
         }, 2000);
     }
     render(){
-        const {isLoading} = this.state;
+        const {isLoading, videos} = this.state;
         if(isLoading)
             return (<Loading msg="cargando..." />)
-        return (<div>Contenido</div>)
+        return (
+            <React.Fragment>
+                <Header />
+                <div className='container'>
+                    {
+                        videos && videos.map((video, i)=>{
+                            return (<Item key={i} data={video}/>)
+                        })
+                    }
+                </div>
+            </React.Fragment>
+        )
     }
 }
 
