5f6d02e247d8129d2047f61eb67ad9e713798a84
josemasf <josemasf@gmail.com>
2022-01-15 14:11:22 +0100

    add details

--- src/api/index.js
+++ src/api/index.js
@@ -6,3 +6,40 @@ export const getVideos = async () => {
 		throw error;
 	}
 }
+
+const getDescription = async () => {
+	try{
+		const resp = await fetch('https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1');
+		return resp.json();
+	}catch(error){
+		throw error;
+	}
+}
+const getVideosById = async (id) => {
+	try{
+		const resp = await fetch(`http://localhost:3500/videos/${id}`);
+		return resp.json();
+	}catch(error){
+		throw error;
+	}
+}
+
+export const getVideoDetail = async ({id}) => new Promise((resolve, reject) => {
+	try{
+		const video = getVideosById(id);
+
+		if(!video) return reject({message:"No se ha encontrado el video ;("});
+		if(video.description) return resolve(video);
+		
+		return getDescription().then(description => {
+			
+			video.description = description.join();
+			
+			
+			return resolve(video);
+		});
+		
+	}catch(error){
+		throw error;
+	}
+});
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
 import { withRouter } from 'react-router'
+import { getVideoDetail } from '../api';
+import Loading from './Loading';
+import Video from './Video';
 
 class Detail extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+        isLoading: true
+      };
+  }
+  componentDidMount() {
+    const { match } = this.props;
+    this.setState({ isLoading: true });
+    getVideoDetail({id: this.props.match.params.id})
+    .then(data => {console.log(data, 'data'); this.setState({ video: data, isLoading: false })})
+    .catch(err => this.setState({ error:err, isLoading: false }));
   }
   render() {
-  
-    return (<React.Fragment> HOLA {this.props.match.params.id}</React.Fragment>);
+    const { isLoading, error, video } = this.state;
+    if (error) return <p className="error">{error.message}</p>;
+    if (isLoading || !video) return <Loading msg={`Cargando video (#${this.props.match.params.id}) .... `} />;
+
+    return (<React.Fragment>        
+        <div className="detail-container">
+            <Video name={video.title} embed={video.embed}/>
+            <div className="detail-summary">
+              <h2 className="detail-title">{video.title}</h2>
+              <p>{video.description}</p>
+            </div>
+        </div>
+     </React.Fragment>);
   }
 }
 
